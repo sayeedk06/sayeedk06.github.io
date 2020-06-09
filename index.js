7324c62e7658cf35bcc2d111dc45704fa8099e11
sayeedk06 <sayeed.shaiban@northsouth.com>
2020-06-09 21:24:26 +0600

    added functionality for about and work showing alternately in the nav bar

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-document.getElementById('date').innerHTML = Date()
+document.getElementById('date').innerHTML = Date();
+
+function showAbout() {
+  document.getElementById('allinfo').style.visibility = "hidden";
+  alert("working");
+  document.getElementById('about').style.visibility = "visible"
+}
+
+
+function showWork() {
+  document.getElementById('allinfo').style.visibility = "visible";
+  document.getElementById('about').style.visibility = "hidden"
+}
+
+
+document.getElementById('nav-about').addEventListener("click", showAbout);
+document.getElementById('nav-work').addEventListener("click", showWork);
+
+// function myFunction() {
+//     alert('It was clicked');
+// }
+// document.getElementById('nav-about').addEventListener('click', myFunction, false);
