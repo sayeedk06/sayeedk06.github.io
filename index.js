12b5a876c9b34fab493fb0f1891cb51dc3cf2e05
sayeedk06 <sayeed.shaiban@northsouth.com>
2020-06-14 00:51:50 +0600

    'Added funky functionality and about pic'

--- index.js
+++ index.js
@@ -2,19 +2,30 @@ document.getElementById('date').innerHTML = Date();
 
 function showAbout() {
   document.getElementById('allinfo').style.visibility = "hidden";
-  alert("working");
-  document.getElementById('about').style.visibility = "visible"
+  document.getElementById('about').style.visibility = "visible";
 }
 
 
 function showWork() {
   document.getElementById('allinfo').style.visibility = "visible";
-  document.getElementById('about').style.visibility = "hidden"
+  document.getElementById('about').style.visibility = "hidden";
 }
 
+function getFunky() {
+document.getElementById('research').style.backgroundColor = "#edf8fa";
+document.getElementById('course').style.backgroundColor = "#e8fcfc";
+document.getElementById('awards').style.backgroundColor = "#f5f6ff";
+
+document.getElementById('research').classList.add("funky");
+document.getElementById('course').classList.add("funky");
+document.getElementById('awards').classList.add("funky");
+document.getElementsByClassName('jumbotron')[0].style.backgroundColor = "violet";
+
+}
 
 document.getElementById('nav-about').addEventListener("click", showAbout);
 document.getElementById('nav-work').addEventListener("click", showWork);
+document.querySelector('button').addEventListener("click", getFunky);
 
 // function myFunction() {
 //     alert('It was clicked');
